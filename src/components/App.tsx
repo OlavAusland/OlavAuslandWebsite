49034ce0ed2a0636246f76c3b7f9720b65ba6d50
Olav Ausland Onstad <olavao@uia.no>
2022-06-03 19:22:02 +0200

    Initialized React-Project

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={myStyle}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
+const myStyle = {
+  color:'black'
+}
