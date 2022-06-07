d6e1e236281ab0cb519490cf09ca9bf36888c151
Olav Ausland Onstad <olavao@uia.no>
2022-06-07 20:11:58 +0200

    Added About Text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,7 +8,12 @@ export default function About(){
                 <h1>About</h1>
                 <hr style={{borderBottomWidth:'1px', width:'100%'}}/>
                 <p style={{alignSelf:'flex-start'}}>
-                    Some Text
+                    Hi! I am a {new Date().getFullYear() - 2001} year old student at the University of Agder in Norway.
+                    I am currently taking a bachelor degree in Computer Science and i want to continue on a masters in Artificial Intelligence.
+
+                    I am currently seeking an internship in the field of Computer Science and software development.&nbsp;
+                    If you want to know more about me, feel free to navigate to the contact page, or contact me on any of my listed platforms. 
+                    If you want to follow up on what i am creating / doing, navigate to the records tab and check out my posts or go to the landing page and look at my repositories.
                 </p>
             </div>
         </div>
