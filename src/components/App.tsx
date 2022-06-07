f7cd347d09056c59ca3bfa79afdb3e0a13a20b54
Olav Ausland Onstad <olavao@uia.no>
2022-06-07 21:29:47 +0200

    Implemented Router Animation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import Home from './Home';
 import NavBar from './NavBar';
 import About from './About';
-import Record from './Record';
+import Record from './Records';
 import Login from './Login';
 import NoRoute from './error/NoRoute';
+import '../styles/app.css';
 
 function App() {
+  const location = useLocation();
   return (
-    <Router>
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/record" element={<Record/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="*" element={<NoRoute/>}/>
-        {/*<Route path="/register" element={<Register/>}/>*/}
-      </Routes>
-    </Router>
+    <Routes location={location}>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/record" element={<Record/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="*" element={<NoRoute/>}/>
+      {/*<Route path="/register" element={<Register/>}/>*/}
+    </Routes>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App';
+import { BrowserRouter as Router,} from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Router>
+    <NavBar/>
     <App />
-  </React.StrictMode>
+  </Router>
 );
